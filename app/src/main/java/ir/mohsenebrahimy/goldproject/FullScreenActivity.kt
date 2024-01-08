4c93f4c262bd227bc9d27387e8c300d4663962c1
mohsenebrahimyir <mohsenebrahimy.ir@mail.com>
2024-01-08 17:09:38 +0330

    fix Deprecation warning

--- app/src/main/java/ir/mohsenebrahimy/goldproject/FullScreenActivity.kt
+++ app/src/main/java/ir/mohsenebrahimy/goldproject/FullScreenActivity.kt
@@ -1,21 +1,29 @@
 package ir.mohsenebrahimy.goldproject
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
+import android.view.WindowInsets
 import android.view.WindowManager
 import ir.mohsenebrahimy.goldproject.databinding.ActivityFullScreenBinding
+
 
 class FullScreenActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFullScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
         binding = ActivityFullScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
     }
 }
